a4571d9e5305b5de3b90c4793fbe483ac7543c66
Oleg <worldedit.72@gmail.com>
2018-06-24 19:03:57 +0500

    insert for kladr works fine

--- Kladr/src/main.py
+++ Kladr/src/main.py
@@ -31,21 +31,73 @@ def q_types(s_title, title, level, kladr_code):
         .format(s_title, title, level, kladr_code)))
 
 
-def q_objects(title, code, ocatd, index, type_id, parent_id, deleted):
-    return ("""INSERT INTO wt_kladr_objects (id, title, kladr_code, kladr_ocatd, kladr_index, type_id, parent_id, deleted)
-         VALUES (DEFAULT, %s, %s, %s, %s, %s, %s, %s) 
-         ON CONFLICT (kladr_code) 
-         DO UPDATE SET 
-         short_title=EXCLUDED.short_title, title=EXCLUDED.title, kladr_code=EXCLUDED.kladr_code; """
-            .format(title, code, ocatd, index, type_id, parent_id, deleted))
+def q_objects(title, short_title, code, ocatd, index, parent_id, deleted, obj_level):
+    return ("""
+        INSERT INTO wt_kladr_objects (id, title, kladr_code, kladr_ocatd, kladr_index, type_id, parent_id, deleted)
+         SELECT nextval('wt_kladr_objects_id_seq'), '{}', '{}', '{}', '{}', id, '{}', '{}'
+            from wt_kladr_types as types
+            where short_title = '{}' and level = '{}';
+             """.format(title, code, ocatd, index, parent_id, deleted, short_title, obj_level))
 
 
-def update_objects_table(cur, dbf_path):
-    table = DBF(filename=dbf_path, encoding='cp866', load=True)
-    records = table.records
-    sorted(records, key=lambda x: x['CODE'])
+def get_kladr_level(code):
+    if code[-5:-2] != '000':
+        lvl = 4
+    elif code[5:8] != '000':
+        lvl = 3
+    elif code[2:5] != '000':
+        lvl = 2
+    else: lvl = 1
+
+    return lvl
+
+
+def update_kladr(conn, cur, dbf_path):
+    table = DBF(filename=dbf_path, encoding='cp866')
+    #records = table.records
+    #sorted(records, key=lambda x: x['CODE'])
+    # for record in table:
+    #     cur.execute(q_objects(record['NAME'], record['SOCR'], record['CODE'], record['OCATD'], record['INDEX'],
+    #                           '1', 'false', get_kladr_level(record['CODE'])))
     for record in table:
-        cur.execute(q_types(record['SOCR'],record['NAME'], record['STATUS'], record['CODE']))
+        cur.execute("""UPDATE wt_kladr_objects
+            SET parent_id = '{}'
+            where kladr_code = '{}';
+            """.format(get_parent_id(cur, record['CODE']), record['CODE']))
+
+
+
+
+def get_parent_id(cur, code):
+    digits_num = 11
+    lvl = get_kladr_level(code)
+    if lvl == 4:
+        r = 8
+    elif lvl == 3:
+        r = 5
+    elif lvl == 2:
+        r = 2
+    else: return 1
+
+    p_code = code[:r] + '0'*(digits_num-r) + '__'
+    q = """SELECT id from wt_kladr_objects
+    WHERE kladr_code LIKE '{}';
+    """
+    cur.execute(q.format(p_code))
+    id = cur.fetchone()
+    if id is None:
+        if lvl == 4:
+            r = 5
+        elif lvl == 3:
+            r = 2
+        p_code = code[:r] + '0'*(digits_num-r) + '__'
+        cur.execute(q.format(p_code))
+        id = cur.fetchone()
+        if id is None:
+            p_code = code[:2] + '0'*(digits_num-2) + '__'
+            cur.execute(q.format(p_code))
+            id = cur.fetchone()
+    return id[0]
 
 
 def update_types(cur, dbf_path):
@@ -68,9 +120,9 @@ def main(url, hostname, db, user, pswd, port, path):
     update_types(cur, arch_path_dir + '/SOCRBASE.DBF')
     print('update wt_kladr_types completed.')
 
-    # print('updating wt_kladr_types from KLADR...')
-    # update_objects_table(cur, arch_path_dir + '/KLADR.DBF')
-    # print('update KLADR completed.')
+    print('updating wt_kladr_types from KLADR...')
+    update_kladr(conn, cur, arch_path_dir + '/KLADR.DBF')
+    print('update KLADR completed.')
     #
     # print('updating wt_kladr_types from STREET...')
     # update_objects_table(cur, arch_path_dir + '/STREET.DBF')
